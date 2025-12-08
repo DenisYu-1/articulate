4eca09bfcc01b6ecad55356368a0ca5009b00bce
denis.yuryev <denis.yuryev@check24.de>
2025-12-08 09:22:07 +0100

    small refactoring

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -15,11 +15,14 @@ abstract class AbstractTestCase extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->sqliteConnection = new Connection('sqlite::memory:', '1', '2');
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../'); // Adjust the path if needed
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
         $dotenv->load();
-        $this->mysqlConnection = new Connection('mysql:host='.$_ENV['DATABASE_HOST'].';dbname=' . $_ENV['DATABASE_NAME'] . ';charset=utf8mb4', $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
-        $this->pgsqlConnection = new Connection('pgsql:host=pgsql;port=5432;dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+
+        $databaseName = $this->getDatabaseName();
+
+        $this->sqliteConnection = new Connection('sqlite::memory:', '1', '2');
+        $this->mysqlConnection = new Connection('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $databaseName . ';charset=utf8mb4', $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+        $this->pgsqlConnection = new Connection('pgsql:host=pgsql;port=5432;dbname=' . $databaseName, $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
 
         $this->sqliteConnection->beginTransaction();
         $this->mysqlConnection->beginTransaction();
@@ -33,5 +36,10 @@ abstract class AbstractTestCase extends TestCase
         $this->sqliteConnection->rollbackTransaction();
         unset($this->sqliteConnection, $this->mysqlConnection, $this->pgsqlConnection);
         parent::tearDown();
+    }
+
+    protected function getDatabaseName(): string
+    {
+        return $_ENV['DATABASE_NAME'];
     }
 }
