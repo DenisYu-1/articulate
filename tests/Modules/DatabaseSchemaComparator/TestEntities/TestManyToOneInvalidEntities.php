8bd1c7c511a79f7ad53667e41829de492f8824f5
denis.yuryev <denis.yuryev@check24.de>
2025-12-09 15:56:35 +0100

    Add test entities for invalid many-to-one and one-to-many relation configurations
    Introduced test entity classes to validate misconfigured `ManyToOne` and `OneToMany` relations, covering scenarios such as missing inverse side, mismatched `mappedBy` references, and incorrect owner types.

---
+++ tests/Modules/DatabaseSchemaComparator/TestEntities/TestManyToOneInvalidEntities.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Articulate\Tests\Modules\DatabaseSchemaComparator\TestEntities;
+
+use Articulate\Attributes\Entity;
+use Articulate\Attributes\Property;
+use Articulate\Attributes\Relations\ManyToOne;
+use Articulate\Attributes\Relations\OneToMany;
+
+#[Entity]
+class TestManyToOneTargetMissingInverse
+{
+    #[Property]
+    public int $id;
+}
+
+#[Entity]
+class TestManyToOneOwnerMissingInverse
+{
+    #[Property]
+    public int $id;
+
+    #[ManyToOne(inversedBy: 'missingProperty')]
+    public TestManyToOneTargetMissingInverse $target;
+}
+
+#[Entity]
+class TestManyToOneTargetMappedByMismatch
+{
+    #[Property]
+    public int $id;
+
+    #[OneToMany(mappedBy: 'otherProperty', targetEntity: TestManyToOneOwnerMappedByMismatch::class)]
+    public TestManyToOneOwnerMappedByMismatch $owners;
+}
+
+#[Entity]
+class TestManyToOneOwnerMappedByMismatch
+{
+    #[Property]
+    public int $id;
+
+    #[ManyToOne(inversedBy: 'owners')]
+    public TestManyToOneTargetMappedByMismatch $targetMismatch;
+}
+
+#[Entity]
+class TestManyToOneTargetInverseIsOwning
+{
+    #[Property]
+    public int $id;
+
+    #[ManyToOne]
+    public TestManyToOneOwnerInverseIsOwning $owners;
+}
+
+#[Entity]
+class TestManyToOneOwnerInverseIsOwning
+{
+    #[Property]
+    public int $id;
+
+    #[ManyToOne(inversedBy: 'owners')]
+    public TestManyToOneTargetInverseIsOwning $target;
+}
+
---
+++ tests/Modules/DatabaseSchemaComparator/TestEntities/TestOneToManyInvalidEntities.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Articulate\Tests\Modules\DatabaseSchemaComparator\TestEntities;
+
+use Articulate\Attributes\Entity;
+use Articulate\Attributes\Property;
+use Articulate\Attributes\Relations\ManyToOne;
+use Articulate\Attributes\Relations\OneToMany;
+
+#[Entity]
+class TestOneToManyInverseMissingOwner
+{
+    #[Property]
+    public int $id;
+
+    #[OneToMany(mappedBy: 'missingOwner', targetEntity: TestManyToOneOwner::class)]
+    public TestManyToOneOwner $items;
+}
+
+#[Entity]
+class TestOneToManyWrongOwnerType
+{
+    #[Property]
+    public int $id;
+
+    #[OneToMany(mappedBy: 'target', targetEntity: TestOneToManyWrongOwner::class)]
+    public TestOneToManyWrongOwner $items;
+}
+
+#[Entity]
+class TestOneToManyWrongOwner
+{
+    #[Property]
+    public int $id;
+
+    #[Property]
+    public int $target;
+}
+
